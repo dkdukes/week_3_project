3fb6cfed55f29caf0d46a2a48d8dd2164c603e7a
dkdukes <fedulike@gmail.com>
2026-06-14 11:24:07 +0300

    add a function to help in currency conversion

--- jumia.py
+++ jumia.py
@@ -4,6 +4,18 @@ import pandas as pd
 
 base_url = "https://www.jumia.co.ke/ultrabooks/"
 
+def currency_convertor(amount,base_currency,target_currency):
+    api_key = "6b286fed0ca4b25d316d8582"
+    api_url = f"https://v6.exchangerate-api.com/v6/{api_key}/pair/{base_currency}/{target_currency}/{amount}"
+    api_response = requests.get(api_url)
+    data = api_response.json()
+    if data["result"] == "success":
+        return data["conversion_result"]
+    else:
+        print("unable to do the conversion! Please check your Url")
+        return None
+
+
 response = requests.get(base_url)
 
 products_list = []
@@ -13,12 +25,13 @@ if response.status_code == 200:
     cards = soup.select(".core")
     for card in cards:
         title = card.select_one(".name").text
-        price = card.select_one(".prc").text
+        price = card.select_one(".prc").text.replace("KSh","").replace(",","")
+        price_in_usd = currency_convertor(int(price),"KES","USD")
         product_details = {
             "Title" : title,
-            "Price" : price.replace("KSh",""),
+            "Price" : price,
+            "USD_Price" : price_in_usd ,
             "Source_URL" :base_url
         }
         products_list.append(product_details)
 
-print(products_list)
